270603dfb10eb06556b57ad686568d8bc803d61c
Ross Prokopchenko <rosskakashka@gmail.com>
2022-09-13 12:50:45 -0400

    fix mismatch err

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from './App';
 
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
